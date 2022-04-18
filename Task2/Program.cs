7a3ab674fe09c53dc07087e382414443a5f2d6e3
Ekaterina <ekaterina_r21@mail.ru>
2022-04-18 17:21:48 +0300

    Задача 13

---
+++ Task2/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+
+if (num / 100 > 0) 
+{
+    Console.WriteLine($"Третьей цифрой числа {num} является цифра {num / 100 % 10}");
+}
+else
+{
+    Console.WriteLine("Третьей цифры нет");
+}
