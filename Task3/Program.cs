7c223053bf2015a28fccd4f7e950648e50722cc6
Ekaterina <ekaterina_r21@mail.ru>
2022-04-18 19:09:22 +0300

    Задача 15

---
+++ Task3/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите номер дня недели: ");
+int dayNum = int.Parse(Console.ReadLine());
+string[] Days = { "понедельник", "вторник", "среда", "четверг", "пятница", "суббота", "воскресенье" };
+string result = "";
+
+if (dayNum > 5) 
+{
+     Console.WriteLine("выходной день");
+}
+else
+{
+    Console.WriteLine("рабочий день");
+}
+System.Console.WriteLine($"{dayNum}-й день недели - это {Days[dayNum - 1]}{result}");
